532ab61a378606366f158036ecf512b2c0918c19
Ulrik de Muelenaere <ulrikdem@gmail.com>
2018-05-08 15:02:40 +0200

    Update tests

--- test/user-login.js
+++ test/user-login.js
@@ -3,13 +3,6 @@ const app = require("../src/app.js")(db);
 const request = require("supertest");
 
 describe("user login", function() {
-	it("should accept valid login credentials", function(done) {
-		request(app)
-			.post("/user/login")
-			.send({username: "dummy", password: "dummy"})
-			.expect(200, {success: true}, done);
-	});
-
 	it("should deny invalid login credentials", function(done) {
 		request(app)
 			.post("/user/login")

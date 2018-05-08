aa8d63577c5b9a556d20e6ea60ee8caed3169529
Ulrik de Muelenaere <ulrikdem@gmail.com>
2018-05-08 18:39:41 +0200

    Update tests

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const db = require("./src/db-mysql.js");
+const db = require("./src/database.js");
 const app = require("./src/app.js")(db);
 
 app.listen(8080); // TODO: Use HTTPS.
---
+++ test/addition.js
@@ -0,0 +1,7 @@
+const assert = require("assert");
+
+describe("addition", function() {
+	it("should work", function() {
+		assert.equal(1 + 1, 2);
+	});
+});

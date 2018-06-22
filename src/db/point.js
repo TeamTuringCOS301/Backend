e326b29e8a0be5db31d7301734cbaaac0fd28e2c
KylePretorius <kylepretorius28@gmail.com>
2018-06-22 15:19:42 +0200

    Fixed refactoring mistakes

--- src/db/point.js
+++ src/db/point.js
@@ -1,5 +1,5 @@
 module.exports = (query) => ({
-	async add(point, area, user, time){
+	async add(point, area, userId, time){
 		await query(`
 			UPDATE tblUser
 			SET usrLastPointTime = ?
@@ -15,12 +15,16 @@ module.exports = (query) => ({
 	async countNearbyPoints(point, id) {
 		const limit = 0.1; // TODO: Update limit.
 		const results = await query(`
-			SELECT a = SIN(RADIANS(cupLocationLatitude - ?) / 2)
-					* SIN(RADIANS(cupLocationLatitude - ?) / 2)
-				+ COS(RADIANS(cupLocationLatitude)) * COS(RADIANS(?))
-					* SIN(RADIANS(cupLocationLongitude - ?) / 2)
-					* SIN(RADIANS(cupLocationLongitude - ?) / 2)
-			WHERE tblConservationArea_conID = ? AND 6371 * ATAN2(SQRT(a), SQRT(1 - a)) < ?`
+			SELECT a
+			FROM (
+				SELECT SIN(RADIANS(cupLocationLatitude - ?) / 2)
+						* SIN(RADIANS(cupLocationLatitude - ?) / 2)
+					+ COS(RADIANS(cupLocationLatitude)) * COS(RADIANS(?))
+						* SIN(RADIANS(cupLocationLongitude - ?) / 2)
+						* SIN(RADIANS(cupLocationLongitude - ?) / 2) AS a
+				FROM tblConservationAreaUserPoints
+				WHERE tblConservationArea_conID = ?) AS alias
+			WHERE 6371 * ATAN2(SQRT(alias.a), SQRT(1 - alias.a)) < ?`,
 			[point.lat, point.lat, point.lat, point.lng, point.lng, id, limit]);
 		return results.length;
 	},
@@ -40,5 +44,41 @@ module.exports = (query) => ({
 					ORDER BY cupDateTime)`,
 				[results.length - limit]);
 		}
+	},
+
+	async list(id){
+		const results = await query(`
+			SELECT cupDateTime,cupLocationLatitude,cupLocationLongitude
+			FROM tblConservationAreaUserPoints
+			WHERE tblConservationArea_conID = ?`,
+			[id]);
+
+		const points=[];
+		for(let point of results){
+			points.push({
+				time: point.cupDateTime,
+				lat: point.cupLocationLatitude,
+				lng: point.cupLocationLongitude
+			});
+		}
+		return points;
+	},
+
+	async listSince(id,since){
+		const results = await query(`
+			SELECT cupDateTime,cupLocationLatitude,cupLocationLongitude
+			FROM tblConservationAreaUserPoints
+			WHERE tblConservationArea_conID = ? AND cupDateTime < ?`,
+			[id,since]);
+
+		const points=[];
+		for(let point of results){
+			points.push({
+				time: point.cupDateTime,
+				lat: point.cupLocationLatitude,
+				lng: point.cupLocationLongitude
+			});
+		}
+		return points;
 	}
 });

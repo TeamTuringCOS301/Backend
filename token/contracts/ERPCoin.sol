8b56afc66a05409dfab5231bd296996ff8d40a9c
Ulrik de Muelenaere <ulrikdem@gmail.com>
2018-09-06 20:33:21 +0200

    Add smart contract function to buy reward

--- token/contracts/ERPCoin.sol
+++ token/contracts/ERPCoin.sol
@@ -5,12 +5,19 @@ import "./StandardToken.sol";
 contract ERPCoin is StandardToken("ERP Coin", "ERP", 0, 0) {
 	address public owner;
 
+	event Purchase(address indexed buyer, uint indexed reward, uint value);
+
 	constructor() public {
 		owner = msg.sender;
 	}
 
-	function rewardCoins(address to, uint value) public returns (bool) {
+	function rewardCoins(address to, uint value) public {
 		require(msg.sender == owner && to != 0x0);
-		return doTransfer(0x0, to, value);
+		doTransfer(0x0, to, value);
+	}
+
+	function buyReward(uint reward, uint value) public {
+		doTransfer(msg.sender, 0x0, value);
+		emit Purchase(msg.sender, reward, value);
 	}
 }
